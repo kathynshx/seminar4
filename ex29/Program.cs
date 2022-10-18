d22539139372415d62b1a4dab83ad955b0957502
Ekaterina Shvalyuk <ekaterinashvalyuk@AirdiEkaterina.lan>
2022-10-18 17:37:28 +0200

    ex29

---
+++ ex29/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+int[] mas = {1,2,5,7,19,6,1,33};
+PrintArray(mas);
+
+void PrintArray(int[] array)
+{
+    for (int i= 0;i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i< array.Length -1)
+        {
+            Console.Write(",");
+        }
+    }
+}
